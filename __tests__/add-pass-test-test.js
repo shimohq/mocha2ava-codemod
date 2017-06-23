5d6e4b5a3383bb5484babfbe9059dd65de7b5941
Lucas Woo <legendecas@gmail.com>
2017-06-23 11:32:10 +0800

    directory structure

--- __tests__/add-pass-test-test.js
+++ __tests__/add-pass-test-test.js
@@ -1,5 +1,4 @@
 jest.autoMockOff();
 const defineTest = require('jscodeshift/src/testUtils').defineTest;
-const transform = require('../add-pass-test');
 
-defineTest(__dirname, 'add-pass-test');
+defineTest(__dirname, 'lib/add-pass-test');
--- __tests__/mocha2ava-test.js
+++ __tests__/mocha2ava-test.js
@@ -1,5 +1,4 @@
 jest.autoMockOff();
 const defineTest = require('jscodeshift/src/testUtils').defineTest;
-const transform = require('../mocha2ava');
 
-defineTest(__dirname, 'mocha2ava');
+defineTest(__dirname, 'lib/mocha2ava');
